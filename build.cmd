650e96b76a90e5c7cd48d6fc739b5366f0dd7770
ramblinwreck35 <japatel@incomm.com>
2016-08-22 13:37:19 -0700

    Made nuget config a little bit more robust (?).

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 cls
-if not exist "packages\FAKE" nuget install FAKE -OutputDirectory packages -ExcludeVersion
+if not exist "packages\FAKE" nuget install -Source "NuGet.org" FAKE -OutputDirectory packages -ExcludeVersion
 packages\FAKE\tools\Fake.exe build.fsx
 REM pause
