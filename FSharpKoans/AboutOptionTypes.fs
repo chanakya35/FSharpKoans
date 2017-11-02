a7e0960324b8f61b1f6137ad73c21c2d8b89256e
Janak <janakchat@gmail.com>
2017-11-01 21:11:36 -0700

    fixed FSharp core version; worked through option types, discriminated unions, modules, and classes.

--- FSharpKoans/AboutOptionTypes.fs
+++ FSharpKoans/AboutOptionTypes.fs
@@ -53,8 +53,8 @@ module ``about option types`` =
             | Some score -> translate score
             | None -> "Unknown"
 
-        AssertEquality (getScore chronoTrigger) __
-        AssertEquality (getScore halo) __
+        AssertEquality (getScore chronoTrigger) "Great"
+        AssertEquality (getScore halo) "Unknown"
 
     [<Koan>]
     let ProjectingValuesFromOptionTypes() =
@@ -67,5 +67,5 @@ module ``about option types`` =
             |> Option.map (fun score -> if score > 3 then "play it" else "don't play")
 
         //HINT: look at the return type of the decide on function
-        AssertEquality (decideOn chronoTrigger) __
-        AssertEquality (decideOn halo) __
+        AssertEquality (decideOn chronoTrigger) (Some "play it")
+        AssertEquality (decideOn halo) None
